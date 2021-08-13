919cdf66ebb2b61424bb0712306ba5a4a87b387b
Konstantin <kostr2010@gmail.com>
2021-08-13 16:36:32 +0300

    Changed Bot Class

--- Bot.cpp
+++ Bot.cpp
@@ -1,39 +1,37 @@
 #include <stdio.h>
-#include <tgbot/tgbot.h>
 #include "SaveFile/SaveFile.h"
+#include "BotClass.h"
+// bool InitCommandsBotMain(bot) {
+//     bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) { 
+//         bot.getApi().sendMessage(message->chat->id, "Hi!"); 
+//     });
+// }
+
 int main()
 {
-    // ====================
-    // SAVE FILE CONFIG
-
-    // save file workflow
     SaveFile Save;
 
-    Save.ReadSave("../BotSettings.txt");
-    Save.AddProperty("Status");
-    Save.SetProperty("Status", "Calm");
-    Save.AddProperty("Token");
-    Save.SetProperty("Token", "1940339152:AAH8fohu8_cETEVz-LNOBEAKT2a5VEJ0Q-8");
-    Save.AddProperty("Config");
-    Save.SetProperty("Config", "0");
-    Save.MakeSave("../BotSettings.txt");
-    return 0;
-
-    // ====================
-    // BOT BODY
-
     TgBot::Bot bot(Save.ReadProperty("Token"));
-    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) { 
-        bot.getApi().sendMessage(message->chat->id, "Hi!"); 
-    });
+    
 
     bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message)    {
-        printf("User wrote %s\n", message->text.c_str());
-        if (StringTools::startsWith(message->text, "/start"))
-        {
-            return;
+
+        switch (state) {
+            case DEFAULT:
+
+            break;
+
+            deafult:
+
+                break;
+
         }
-        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+        // printf("User wrote %s\n", message->text.c_str());
+        // if (StringTools::startsWith(message->text, "/start"))
+        // {
+        //     return;
+        // }
+        // bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
     });
     
     printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
