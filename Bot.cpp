61180efe05c5fd254869f1b74d72d2673fc1e61e
Konstantin <kostr2010@gmail.com>
2021-08-13 15:43:44 +0300

    Throw trash from file

--- Bot.cpp
+++ Bot.cpp
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include <tgbot/tgbot.h>
-
+#include "SaveFile/SaveFile.h"
 int main()
 {
     // ====================
     // SAVE FILE CONFIG
 
     // save file workflow
+    SaveFile Save;
+
+    Save.ReadSave("../BotSettings.txt");
+    Save.AddProperty("Status");
+    Save.SetProperty("Status", "Calm");
+    Save.AddProperty("Token");
+    Save.SetProperty("Token", "1940339152:AAH8fohu8_cETEVz-LNOBEAKT2a5VEJ0Q-8");
+    Save.AddProperty("Config");
+    Save.SetProperty("Config", "0");
+    Save.MakeSave("../BotSettings.txt");
+    return 0;
 
     // ====================
     // BOT BODY
 
-    TgBot::Bot bot("1940339152:AAH8fohu8_cETEVz-LNOBEAKT2a5VEJ0Q-8");
+    TgBot::Bot bot(Save.ReadProperty("Token"));
     bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) { 
         bot.getApi().sendMessage(message->chat->id, "Hi!"); 
     });
