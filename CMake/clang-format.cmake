3c7a01505be5c160ac7521340baa824223485d66
Konstantin <kostr2010@gmail.com>
2021-08-14 13:37:29 +0300

    Config for Cmake

--- CMake/clang-format.cmake
+++ CMake/clang-format.cmake
@@ -2,10 +2,7 @@ set_property(GLOBAL PROPERTY LIST_FILES)
 
 function (add_to_target_clang TARGET) 
     set_property(GLOBAL APPEND PROPERTY LIST_FILES ${TARGET})
+    message("-- Added Target " ${TARGET})
     
 endfunction(add_to_target_clang)
 
-add_custom_target(
-        clang-force-format
-        COMMAND /usr/bin/clang-format -style=webkit -i  ${LIST_FILES}
-)
