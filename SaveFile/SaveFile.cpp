d61fdd3d2d1d8a3dc9789d1c99b70cf79d7bf157
Konstantin <kostr2010@gmail.com>
2021-08-17 13:51:09 +0300

    Library for Files

--- SaveFile/SaveFile.cpp
+++ SaveFile/SaveFile.cpp
@@ -1,7 +1,10 @@
 #include "SaveFile.h"
-
+#include <locale>
+#include <cctype>
+#include <codecvt>
 #include <fstream>
 #include <iostream>
+#include <algorithm>
 //====================
 //Erros Check
 
@@ -11,6 +14,20 @@ bool SaveFile::IsPropertyExists(const std::string& Property) {
         return false;
     }
     return true;
+}
+std::string ToLowerNoSpaces(const std::string& str) {
+    setlocale(LC_CTYPE, "ru_RU.UTF-8");
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> converter;
+    std::wstring ws = converter.from_bytes(str);
+    std::transform(ws.begin(), ws.end(), ws.begin(), std::bind2nd(std::ptr_fun(&std::tolower<wchar_t>), std::locale("ru_RU.UTF-8")));
+
+    // std::remove_if(ws.begin(), ws.end(), isspace);
+    ws.erase(std::remove(ws.begin(), ws.end(), L' '), ws.end());
+    ws.erase(std::remove(ws.begin(), ws.end(), L'\t'), ws.end());
+    ws.erase(std::remove(ws.begin(), ws.end(), L'\n'), ws.end());
+    ws.erase(std::remove(ws.begin(), ws.end(), L'\r'), ws.end());
+
+    return converter.to_bytes(ws);
 }
 
 bool SaveFile::IsExeptable(char Char)
